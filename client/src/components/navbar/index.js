ce3010311f0ca155ec6706f764faec8bae837f01
Jaden Stanton <68559776+Jadenstanton@users.noreply.github.com>
2022-10-01 11:08:57 -0500

    Adding navbar starter

---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <Bars />
+
+            <NavMenu>
+            <NavLink to='/about' activeStyle>
+                About
+            </NavLink>
+            <NavLink to='/services' activeStyle>
+                Services
+            </NavLink>
+            <NavLink to='/contact' activeStyle>
+                Contact
+            </NavLink>
+            <NavLink to='/login' activeStyle>
+                Login
+            </NavLink>
+            <NavLink to='/home' activeStyle>
+                home
+            </NavLink>
+            {/* Second Nav */}
+            {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+            </NavMenu>
+            <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+            </NavBtn>
+        </Nav>
+        </>
+    );
+};
+
+export default Navbar;
