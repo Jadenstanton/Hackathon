db481d1bf4ba4746391952604f622b66a35ad124
Futapo <andre12boy@gmail.com>
2022-10-02 00:26:14 -0500

    new

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../../Components/signin';
+const Login = () => {
+  return (
+  <div className="container">
+    
+    <SignIn/>
+    
+  </div>
+  )
+}
+export default Login;
